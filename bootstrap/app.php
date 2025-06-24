d1a66cd2e525d5cb89ff93752f7be03ecc31ed75
VITOR PEDROSO <vitorsamuel283@gmail.com>
2025-06-24 12:55:54 -0300

    chore: add Laravel Sanctum for auth and L5 Swagger for API documentation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\CheckAbilities;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            // Requer TODAS as permissões listadas no token (condição lógica E)
+            'abilities' => CheckAbilities::class,
+
+            // Requer PELO MENOS UMA das permissões listadas no token (condição lógica OU)
+            'ability' => CheckForAnyAbility::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

f9c6e1de29a8c0d075801e96c56915b45f3d825a
VITOR PEDROSO <vitorsamuel283@gmail.com>
2025-06-26 14:52:38 -0300

    fix: ajuste routes with id

--- routes/api.php
+++ routes/api.php
@@ -10,13 +10,13 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::middleware(['auth:sanctum', 'throttle:120,1'])->group(function () {
-    Route::apiResource('customers', CustomerController::class)->only([
-        'index',
-        'store',
-        'show',
-        'update',
-        'destroy'
-    ]);
+    Route::prefix('customers')->group(function () {
+        Route::get('/', [CustomerController::class, 'index']);
+        Route::post('/', [CustomerController::class, 'store']);
+        Route::get('{id}', [CustomerController::class, 'show']);
+        Route::put('{id}', [CustomerController::class, 'update']);
+        Route::delete('{id}', [CustomerController::class, 'destroy']);
+    });
 
     Route::prefix('customers/{customer}')->group(function () {
         Route::get('favorites', [CustomerFavoriteController::class, 'index']);
